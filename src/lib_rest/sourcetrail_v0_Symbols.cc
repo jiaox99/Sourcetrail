e5fbb8ed3a6057f27a285f70ff6cd7f7c5fdd216
jiaox99 <jiaox99@163.com>
2025-09-11 15:57:13 +0800

    A simple graph query imp

--- src/lib_rest/sourcetrail_v0_Symbols.cc
+++ src/lib_rest/sourcetrail_v0_Symbols.cc
@@ -3,23 +3,64 @@
 #include "StorageAccess.h"
 #include "boost/locale.hpp"
 #include "NodeTypeSet.h"
+#include "Graph.h"
+#include "NameHierarchy.h"
 
 using namespace sourcetrail::v0;
+using namespace drogon;
+
+std::wstring to_wstring(const std::string& str)
+{
+	return boost::locale::conv::utf_to_utf<wchar_t>(str);
+}
+
+std::string to_string(const std::wstring& wstr)
+{
+	return boost::locale::conv::utf_to_utf<char>(wstr);
+}
 
 // Add definition of your processing function here
 void Symbols::fuzzyQuery(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback, const std::string& query) const
 {
 	int limit = req->getOptionalParameter<int>("max").value_or(50);
 	auto *storage = getStorageInstance();
-	auto result = storage->getAutocompletionMatches(boost::locale::conv::utf_to_utf<wchar_t>(query), NodeTypeSet::all(), false);
+	auto result = storage->getAutocompletionMatches(to_wstring(query), NodeTypeSet::all(), false);
 	Json::Value jsonResult;
 	int count = 0;
 	for (const auto& entry: result)
 	{
-		jsonResult.append(boost::locale::conv::utf_to_utf<char>(entry.getFullName()));
-		if (++count >= limit)
-			break;
+		for (const auto& tokenName : entry.tokenNames)
+		{
+			jsonResult.append(to_string(NameHierarchy::serialize(tokenName)));
+			if (++count >= limit)
+				break;
+		}
 	}
 	auto resp = HttpResponse::newHttpJsonResponse(jsonResult);
+	callback(resp);
+}
+
+void Symbols::graphQuery(const SymbolQueryRequest&& query, std::function<void(const drogon::HttpResponsePtr&)>&& callback) const
+{
+	auto *storage = getStorageInstance();
+	
+	std::vector<Id> symbolIds;
+	for (const auto& symbolFullName: query.symbolFullNames)
+	{
+		NameHierarchy symbolHierarchy = NameHierarchy::deserialize(to_wstring(symbolFullName));
+		auto symbolId = storage->getNodeIdForNameHierarchy(symbolHierarchy);
+		if (symbolId != 0)
+			symbolIds.push_back(symbolId);
+	}
+	std::vector<Id> expandedNodeIds;
+	bool isNameSpace = false;
+	auto graph = storage->getGraphForActiveTokenIds(symbolIds, expandedNodeIds, &isNameSpace);
+
+	auto resp = HttpResponse::newHttpResponse();
+	resp->setContentTypeCode(CT_TEXT_PLAIN);
+	resp->setStatusCode(k200OK);
+	std::wstringstream wss;
+	graph->print(wss);
+	resp->setBody(to_string(wss.str()));
 	callback(resp);
 }
--- src/lib_rest/sourcetrail_v0_Symbols.h
+++ src/lib_rest/sourcetrail_v0_Symbols.h
@@ -2,20 +2,43 @@
 
 #include <drogon/HttpController.h>
 
-using namespace drogon;
+struct SymbolQueryRequest
+{
+	std::vector<std::string> symbolFullNames;
+};
+
+namespace drogon
+{
+
+template <>
+inline SymbolQueryRequest fromRequest(const HttpRequest& req)
+{
+	auto json = req.getJsonObject();
+	SymbolQueryRequest queryReq;
+	for (auto& symbolFullName : (*json)["symbolFullNames"])
+	{
+		queryReq.symbolFullNames.push_back(symbolFullName.asString());
+	}
+	return queryReq;
+}
+
+}
 
 namespace sourcetrail
 {
 namespace v0
 {
+
 class Symbols : public drogon::HttpController<Symbols>
 {
   public:
     METHOD_LIST_BEGIN
-    METHOD_ADD(Symbols::fuzzyQuery, "/fuzzyQuery/{}", Get);
+    METHOD_ADD(Symbols::fuzzyQuery, "/fuzzyQuery/{}", drogon::Get);
+	METHOD_ADD(Symbols::graphQuery, "/graphQuery", drogon::Post);
     METHOD_LIST_END
 private:
-	void fuzzyQuery(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback, const std::string& query) const;
+	void fuzzyQuery(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, const std::string& query) const;
+	void graphQuery(const SymbolQueryRequest&& query, std::function<void(const drogon::HttpResponsePtr&)>&& callback) const;
 };
 }
 }
