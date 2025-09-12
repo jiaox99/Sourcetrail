816976734528c515589ec3bf4c596cc241c00e36
jiaox99 <jiaox99@163.com>
2025-09-12 09:14:33 +0800

    Custom graph query impl

--- src/lib_rest/sourcetrail_v0_Symbols.cc
+++ src/lib_rest/sourcetrail_v0_Symbols.cc
@@ -46,18 +46,47 @@ void Symbols::fuzzyQuery(const HttpRequestPtr& req, std::function<void(const Htt
 	callback(resp);
 }
 
-void Symbols::graphQuery(const SymbolQueryRequest&& query, std::function<void(const drogon::HttpResponsePtr&)>&& callback) const
+void errorResponse(const std::string& message, std::function<void(const drogon::HttpResponsePtr&)>& callback)
 {
-	auto *storage = getStorageInstance();
-	
-	std::vector<Id> symbolIds;
-	for (const auto& symbolFullName: query.symbolFullNames)
+	auto resp = HttpResponse::newHttpResponse();
+	resp->setContentTypeCode(CT_TEXT_PLAIN);
+	resp->setStatusCode(k200OK);
+	resp->setBody(message);
+	callback(resp);
+}
+
+bool validateQueryRequest(const SymbolQueryRequest& query, std::function<void(const drogon::HttpResponsePtr&)>& callback)
+{
+	if (query.symbolFullNames.empty())
+	{
+		errorResponse("No symbolFullNames provided", callback);
+		return false;
+	}
+
+	return true;
+}
+
+void symbolNameToIds(const std::vector<std::string>& symbolFullNames, std::vector<Id>& outSymbolIds, StorageAccess* storage)
+{
+	for (const auto& symbolFullName: symbolFullNames)
 	{
 		NameHierarchy symbolHierarchy = NameHierarchy::deserialize(to_wstring(symbolFullName));
 		auto symbolId = storage->getNodeIdForNameHierarchy(symbolHierarchy);
 		if (symbolId != 0)
-			symbolIds.push_back(symbolId);
+			outSymbolIds.push_back(symbolId);
 	}
+}
+
+void Symbols::graphQuery(const SymbolQueryRequest&& query, std::function<void(const drogon::HttpResponsePtr&)>&& callback) const
+{
+	if (!validateQueryRequest(query, callback))
+	{
+		return;
+	}
+
+	auto *storage = getStorageInstance();
+	std::vector<Id> symbolIds;
+	symbolNameToIds(query.symbolFullNames, symbolIds, storage);
 	std::vector<Id> expandedNodeIds;
 	bool isNameSpace = false;
 	auto graph = storage->getGraphForActiveTokenIds(symbolIds, expandedNodeIds, &isNameSpace);
@@ -69,4 +98,45 @@ void Symbols::graphQuery(const SymbolQueryRequest&& query, std::function<void(co
 	graph->print(wss);
 	resp->setBody(to_string(wss.str()));
 	callback(resp);
+}
+
+NodeKindMask fromNodeKindStrings(const std::vector<std::string>& nodeKinds)
+{
+	NodeKindMask mask = 0;
+	for (const auto& nodeKindStr: nodeKinds)
+	{
+		mask |= getNodeKindForReadableNodeKindString(to_wstring(nodeKindStr));
+	}
+	mask = mask == 0 ? ~0 : mask;
+	return mask;
+}
+
+Edge::TypeMask fromEdgeTypeStrings(const std::vector<std::string>& edgeTypes)
+{
+	Edge::TypeMask mask = 0;
+	for (const auto& edgeTypeStr: edgeTypes)
+	{
+		mask |= Edge::getTypeForReadableTypeString(to_wstring(edgeTypeStr));
+	}
+
+	mask = mask == 0 ? ~0 : mask;
+	return mask;
+}
+
+void Symbols::customGraphQuery(const SymbolQueryRequest&& query, std::function<void(const drogon::HttpResponsePtr&)>&& callback) const
+{
+	if (!validateQueryRequest(query, callback))
+	{
+		return;
+	}
+
+	auto* storage = getStorageInstance();
+	std::vector<Id> symbolIds;
+	symbolNameToIds(query.symbolFullNames, symbolIds, storage);
+	if (symbolIds.size() != 2)
+	{
+		errorResponse("Two Symbol Names needed", callback);
+		return;
+	}
+	auto graph = storage->getGraphForTrail(symbolIds[0], symbolIds[1], fromNodeKindStrings(query.nodeTypes), fromEdgeTypeStrings(query.edgeTypes), true, query.maxDepth, true);
 }
--- src/lib_rest/sourcetrail_v0_Symbols.h
+++ src/lib_rest/sourcetrail_v0_Symbols.h
@@ -5,6 +5,9 @@
 struct SymbolQueryRequest
 {
 	std::vector<std::string> symbolFullNames;
+	int maxDepth = 5;
+	std::vector<std::string> nodeTypes;
+	std::vector<std::string> edgeTypes;
 };
 
 namespace drogon
@@ -15,10 +18,40 @@ inline SymbolQueryRequest fromRequest(const HttpRequest& req)
 {
 	auto json = req.getJsonObject();
 	SymbolQueryRequest queryReq;
-	for (auto& symbolFullName : (*json)["symbolFullNames"])
+	if (json == nullptr)
 	{
-		queryReq.symbolFullNames.push_back(symbolFullName.asString());
+		return queryReq;
 	}
+
+	if (!json->find("symbolFullNames")->isNull())
+	{
+		for (auto& symbolFullName: (*json)["symbolFullNames"])
+		{
+			queryReq.symbolFullNames.push_back(symbolFullName.asString());
+		}
+	}
+
+	if (!json->find("nodeTypes")->isNull())
+	{
+		for (auto& nodeType: (*json)["nodeTypes"])
+		{
+			queryReq.nodeTypes.push_back(nodeType.asString());
+		}
+	}
+
+	if (!json->find("edgeTypes")->isNull())
+	{
+		for (auto& edgeType: (*json)["edgeTypes"])
+		{
+			queryReq.edgeTypes.push_back(edgeType.asString());
+		}
+	}
+
+	if (!json->find("maxDepth")->isNull())
+	{
+		queryReq.maxDepth = (*json)["maxDepth"].asInt();
+	}
+
 	return queryReq;
 }
 
@@ -35,10 +68,12 @@ class Symbols : public drogon::HttpController<Symbols>
     METHOD_LIST_BEGIN
     METHOD_ADD(Symbols::fuzzyQuery, "/fuzzyQuery/{}", drogon::Get);
 	METHOD_ADD(Symbols::graphQuery, "/graphQuery", drogon::Post);
+	METHOD_ADD(Symbols::customGraphQuery, "/customGraphQuery", drogon::Post);
     METHOD_LIST_END
 private:
 	void fuzzyQuery(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, const std::string& query) const;
 	void graphQuery(const SymbolQueryRequest&& query, std::function<void(const drogon::HttpResponsePtr&)>&& callback) const;
+	void customGraphQuery(const SymbolQueryRequest&& query, std::function<void(const drogon::HttpResponsePtr&)>&& callback) const;
 };
 }
 }
